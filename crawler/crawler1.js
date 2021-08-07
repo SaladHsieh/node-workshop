5426a80f54a36c9712822131e76e32bf27c2e10c
Sarah <hsue1207@gmail.com>
2021-08-07 20:47:56 +0800

    debug 除去stockCode多餘空白.換行字元

--- crawler/crawler1.js
+++ crawler/crawler1.js
@@ -15,16 +15,17 @@ const fs = require('fs');
 
 // console.log(moment().format('YYYYMMDD'));
 
-fs.readFile("stock.txt", "utf8", (err, stockCode) => {
+fs.readFile('stock.txt', 'utf8', (err, stockCode) => {
     if (err) {
         console.log(err);
     } else {
+        console.log(stockCode.trim()); //確認一下stockCode是不是正確
         axios
             .get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
             params: {
                 response:'json',
                 date:moment().format('YYYYMMDD'),
-                stockNo: stockCode,
+                stockNo: stockCode.trim(),  //**注意格式**用.trim()除去空白.換行字元
                 },
             })
             .then((response) => {
