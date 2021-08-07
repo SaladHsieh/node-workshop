100e012dbe26c5695272d8c7f5086bdf134c9e15
Sarah <hsue1207@gmail.com>
2021-08-08 06:09:11 +0800

    增加try catch驗證

--- promise/promise_async_await.js
+++ promise/promise_async_await.js
@@ -21,15 +21,23 @@ console.log(`開始工作 at ${dt.toISOString()}`);
 
 let dailyWork = async () => {  // let 變數 = async () 匿名函式
     // await會等到 Promise 的狀態是 fulfilled 才執行
-    let job1 = await doWork('刷牙', 3000, true);
-    console.log(job1);
-    let job2 = await doWork('洗臉', 2000, true);
-    console.log(job2);
-    let job3 = await doWork('吃早餐', 3000, true);
-    console.log(job3);
-    let job4 = await doWork('運動', 4000, true);
-    console.log(job4);
-    let job5 = await doWork('上課', 2000, true);
-    console.log(job5);
+    try {
+        let job1 = await doWork('刷牙', 3000, true);
+        console.log(job1);
+    } catch (e) {
+        console.error(e);
+    }
+    try {
+        let job2 = await doWork('洗臉', 2000, true);
+        console.log(job2);
+        let job3 = await doWork('吃早餐', 3000, false);
+        console.log(job3);
+        let job4 = await doWork('運動', 4000, true);
+        console.log(job4);
+        let job5 = await doWork('上課', 2000, true);
+        console.log(job5);
+    } catch (e) {
+        console.error(e);
+    }
 }
 dailyWork();
