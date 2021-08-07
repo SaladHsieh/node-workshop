98536e062c5fcdf24764df4c88d1b04914129689
Sarah <hsue1207@gmail.com>
2021-08-07 19:34:56 +0800

    stockNo改用讀取的

--- crawler/crawler1.js
+++ crawler/crawler1.js
@@ -1,19 +1,37 @@
-const axios = require("axios");
-const moment = require("moment");
+// GET
+// https://www.twse.com.tw/exchangeReport/STOCK_DAY
+// ?
+// response=json
+// &
+// date=20210807
+// &
+// stockNo=2330
+
+const axios = require('axios');
+const moment = require('moment');
+
+// fs讀股票代碼
+const fs = require('fs');
 
 // console.log(moment().format('YYYYMMDD'));
 
-axios
-    .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-    params: {
-        response:'json',
-        date:moment().format('YYYYMMDD'),
-        stockNo:'2330'
-        }
-    })
-    .then((response) => {
-        console.log(response.data);
-    })
-    .catch((error) =>{
-        console.log(error);
-    });
+fs.readFile("stock.txt", "utf8", (err, stockCode) => {
+    if (err) {
+        console.log(err);
+    } else {
+        axios
+            .get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
+            params: {
+                response:'json',
+                date:moment().format('YYYYMMDD'),
+                stockNo: stockCode,
+                },
+            })
+            .then((response) => {
+                console.log(response.data.title);
+            })
+            .catch((error) =>{
+                console.log(error);
+            });
+    }
+})
