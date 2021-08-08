6bdbd9be3ab22f1d801431f2c1453c169c1ecfea
Sarah <hsue1207@gmail.com>
2021-08-09 06:33:45 +0800

    promise chain 及格式調整

--- crawler/crawler2.js
+++ crawler/crawler2.js
@@ -1,37 +1,36 @@
-const axios = require('axios');
-const moment = require('moment');
+const axios = require("axios");
+const moment = require("moment");
 
 // fs讀股票代碼
-const fs = require('fs');
+const fs = require("fs");
 
 // console.log(moment().format('YYYYMMDD'));
 
-
 // then catch用法
-new Promise ((resolve, reject) => {
-    fs.readFile('stock.txt', 'utf8', (err, stockCode) => {  //先讀到stockCode
+new Promise((resolve, reject) => {                              // Promise 物件
+    fs.readFile("stock.txt", "utf8", (err, stockCode) => {
+        //先讀到stockCode
         if (err) {
             reject(err);
         } else {
-            resolve(stockCode);
-            console.log(stockCode.trim()); //確認一下stockCode是不是正確
+            resolve(stockCode.trim());
+            // 可以先console.log確認一下stockCode是不是正確
+            // .trim()移除前後空白字元, 包含換行 **收到資料要處理!!很重要!!**
         }
-    })
-}).then((stockCode) => {
-    let stockNo = stockCode.trim();
-    return axios
-        .get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
-        params: {
-            response:'json',
-            date:moment().format('YYYYMMDD'),
-            stockNo,  //**注意格式**用.trim()除去空白.換行字元
-            },
-        })
-        .then((result) => {
-            console.log(result.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+    });
 })
-
+    .then((stockCode) => {
+        return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {    // Promise 物件(axios -> Promise based )
+            params: {
+                response: "json",
+                date: moment().format("YYYYMMDD"),
+                stockNo: stockCode, //**注意格式**用.trim()除去空白.換行字元
+            },
+        });
+    })
+    .then((result) => {
+        console.log(result.data);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
