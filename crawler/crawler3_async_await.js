deadf92227141a46b43f193df2408fa7a5b81fde
Sarah <hsue1207@gmail.com>
2021-08-08 12:52:08 +0800

    debug 成功

--- crawler/crawler3_async_await.js
+++ crawler/crawler3_async_await.js
@@ -6,38 +6,48 @@ const fs = require('fs');
 
 // console.log(moment().format('YYYYMMDD'));
 
-let getStockCode = function () {
+function getStockCode() {
     return new Promise ((resolve, reject) => {
         fs.readFile('stock.txt', 'utf8', (err, stockCode) => {  //先讀到stockCode
             if (err) {
                 reject(err);
             } else {
-                resolve(stockCode);
-                console.log(stockCode.trim());
-                // 確認一下stockCode是不是正確
+                resolve(stockCode.trim());
+                // 移除前後空白自元, 包含換行 **收到資料要處理!!很重要!!**
             }
         })
     });
-}
+};
 
-let readFile = function (stockCode) {
-    axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
+function readFile (stockCode) {
+    return axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
         params: {
             response:'json',
             date:moment().format('YYYYMMDD'),
             stockNo: stockCode,  //**注意格式**用.trim()除去空白.換行字元
             },
     });
-}
+};
 
-// 直接執行
+// 立即函式前一定要分號區隔
 (async function () {
     try {
         let theStock = await getStockCode();
         let getData = await readFile(theStock);
         console.log(getData.data);
     } catch (e) {
-        console.log(e);
+        console.error(e);
     }
 })();
 
+
+// async function result() {
+//     try {
+//         let stockNo = await getStockCode();
+//         let getData = await readFile(stockNo);
+//         console.log(getData.data);
+//     } catch(e) {
+//         console.error(e);
+//     }
+// }
+// result();
