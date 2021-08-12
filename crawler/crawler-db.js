09b5c90d0dfd08884534a8ac4b7dde7e6f85f456
Sarah <hsue1207@gmail.com>
2021-08-12 18:48:04 +0800

    爬蟲讀取資料並寫入database

--- crawler/crawler-db.js
+++ crawler/crawler-db.js
@@ -1,12 +1,12 @@
+// 套件
 const axios = require("axios");
 const moment = require("moment");
-
 // fs讀股票代碼
 const fs = require("fs");
-
 const mysql = require("mysql");
 require("dotenv").config();
 
+// 設定連線資料
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -15,16 +15,12 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME,
 });
 
+// 準備連線
 connection.connect((err) => {
     if (err) {
         console.error("資料庫連不上", err);
     }
 });
-
-// 不關閉連線，認為程式一直在執行
-connection.end();
-
-// console.log(moment().format('YYYYMMDD'));
 
 function getStockCode() {
     return new Promise((resolve, reject) => {
@@ -40,13 +36,51 @@ function getStockCode() {
     });
 }
 
-function readFile(stockCode) {
+function queryStockDataPromise(stockCode) {
     return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
         params: {
             response: "json",
             date: moment().format("YYYYMMDD"),
-            stockNo: stockCode, //**注意格式**用.trim()除去空白.換行字元
+            stockNo: stockCode,
         },
+    });
+}
+
+function queryStockCode(stockCode) {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            "SELECT * FROM `stock` WHERE `stock_id` = ?",
+            [stockCode],
+            function (error, results, fields) {
+                if (error) {
+                    reject(error);
+                }
+                resolve(results);
+            }
+        );
+    });
+}
+
+// fields: [
+//     '日期',     '成交股數',
+//     '成交金額', '開盤價',
+//     '最高價',   '最低價',
+//     '收盤價',   '漲跌價差',
+//     '成交筆數'
+//   ],
+// 照整理好的資料內容插入 stock_price 欄位 stock_id	date open_price	high_price low_price close_price delta_price transactions volume amount
+function insertStockData(parsedData) {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            "INSERT INTO IGNORE stock_price (stock_id, date, volume, amount, open_price, high_price, low_price, close_price, delta_price, transactions) VALUES ?", // IGNORE 忽略重複部分
+            [parsedData],
+            function (error, results, fields) {
+                if (error) {
+                    reject(error);
+                }
+                resolve(results);
+            }
+        );
     });
 }
 
@@ -61,13 +95,53 @@ function readFile(stockCode) {
 //     }
 // })();
 
-// async function result() {
-//     try {
-//         let stockNo = await getStockCode();
-//         let getData = await readFile(stockNo);
-//         console.log(getData.data);
-//     } catch(e) {
-//         console.error(e);
-//     }
-// }
-// result();
+async function result() {
+    try {
+        // 1. 讀 stock.txt 把股票代碼讀進來
+        let stockNo = await getStockCode();
+        // 2. 去資料庫的 stock 表格查看看，這個代碼是不是在我們的服務範圍內
+        let dbResults = await queryStockCode(stockNo);
+        if (dbResults.length === 0) {
+            // console.warn("此股票代碼不在服務範圍內");
+            // return;
+            throw "此股票代碼不在服務範圍內";
+        }
+        console.info("在資料庫有查到資料");
+
+        // 3. 如果是，才去證交所抓資料
+        let getData = await queryStockDataPromise(stockNo);
+
+        // 4. 抓回來的資料存到資料庫的 stock_price 表格裡去
+        // 4-1. data是一個大陣列, 裡面每一個元素也是一個陣列
+        // 4-2. 整理收到的資料格式, 判定stat是不是ok
+        // 4-3. 改西元年(資料庫都是西元年) 拿掉/, 轉成int, +1911
+        // 4-4. 拿掉逗號
+
+        const twseData = getData.data;
+        if (twseData.stat !== "OK") {
+            throw "系統有誤, 請聯絡客服, 或請稍後再試, 謝謝!";
+        }
+        let parsedData = twseData.data.map((item) => {
+            item = item.map((value) => {
+                return value.replace(/,/g, "");
+            });
+            item[0] = parseInt(item[0].replace(/\//g, ""), 10) + 19110000;
+            // console.log(item);
+            // console.log("-----------");
+
+            //插入stock_id
+            item.unshift(stockNo);
+            return item;
+        });
+        console.log(parsedData);
+
+        let insertResult = await insertStockData(parsedData);
+        console.log(insertResult);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        // 不關閉連線，認為程式一直在執行
+        connection.end();
+    }
+}
+result();
