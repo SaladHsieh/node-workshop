58929356f8d078b06b0aee6f024556d7f00dc241
Sarah <hsue1207@gmail.com>
2021-08-15 11:37:21 +0800

    add middleware

--- simple-express/server.js
+++ simple-express/server.js
@@ -3,12 +3,23 @@ const express = require("express");
 // 利用 express 建立了一個 express application
 let app = express();
 
+app.use((req, res, next) => {
+    console.log("This is first line!");
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("This is second line!");
+    next();
+});
+
 // HTTP Method: get, post, put, patch, delete
+// router 路由 -> 比對網址
 app.get("/", function (request, response, next) {
     response.send("Hello");
 });
 app.get("/About", function (request, response, next) {
-    response.send("Home Page");
+    response.send("About Us");
 });
 
 app.listen(3000, function () {
