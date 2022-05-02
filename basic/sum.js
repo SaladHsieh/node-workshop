cfaf8f3f1f5cfbfc6dc319314c887e7181216e9b
Sarah <hsue1207@gmail.com>
2022-05-02 12:58:40 +0800

    XMLHttpRequest Promise 版本

--- basic/sum.js
+++ basic/sum.js
@@ -1,13 +1,13 @@
-console.log("Hello World");
+console.log('Hello World');
 
 // Ｏ(1) bigＯ(1)表示執行的速度跟n大小無關
 // 一行程式碼可以解決
-sum1 = (n) => {
+const sum1 = (n) => {
   return ((n + 1) * n) / 2;
 };
 
 // Ｏ(n) bigＯ(n)表示n越大執行速度越久
-sum = (n) => {
+const sum2 = (n) => {
   let result = 0;
   for (let i = 0; i <= n; i++) {
     result += i;
@@ -15,6 +15,19 @@ sum = (n) => {
   return result;
 };
 
-console.log(sum(1), sum1(1));
-console.log(sum(2), sum1(2));
-console.log(sum(10), sum1(10));
+// console.log(sum1(1), sum2(1));
+// console.log(sum1(2), sum2(2));
+// console.log(sum1(10), sum2(10));
+
+// 效能測試
+console.time('sum1');
+for (let i = 1; i <= 10000; i++) {
+  sum1(1000);
+}
+console.timeEnd('sum1');
+
+console.time('sum2');
+for (let i = 1; i <= 10000; i++) {
+  sum2(1000);
+}
+console.timeEnd('sum2');
